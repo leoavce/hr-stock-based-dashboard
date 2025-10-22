a768722a1d076ecf15fa8098c1b9329356345111
leoavce <hrdeveloper.intern@gmail.com>
2025-10-22 15:38:29 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,44 +1,43 @@
 // js/app.js
+import { spinner } from "./ui.js";
+import { auth } from "./firebase.js";
+
 import { LoginView, bindLogin } from "./views/login.js";
 import { DashboardView, bindDashboard } from "./views/dashboard.js";
 import { GrantsListView, bindGrantsList } from "./views/grants_list.js";
 import { GrantDetailView, bindGrantDetail } from "./views/grant_detail.js";
-import { spinner } from "./ui.js";
-import { auth } from "./firebase.js";
+import { GrantFormView, bindGrantForm } from "./views/admin_grant_form.js";
+import { CsvView, bindCsv } from "./views/admin_csv.js";
 
 const app = document.getElementById("app");
 
-export async function navigate(hash, user = auth.currentUser) {
+export async function navigate(hash, user = auth.currentUser){
   const [_, route, id] = (hash || "").split("/");
   spinner(true);
 
-  if (!user && route !== "login") {
+  if(!user && route !== "login"){
     app.innerHTML = LoginView();
     bindLogin();
     return;
   }
 
-  switch (route) {
+  switch(route){
     case "login":
-      app.innerHTML = LoginView();
-      bindLogin();
-      break;
+      app.innerHTML = LoginView(); bindLogin(); break;
     case "dashboard":
-      app.innerHTML = DashboardView(user);
-      await bindDashboard(user);
-      break;
+      app.innerHTML = DashboardView(user); await bindDashboard(user); break;
     case "grants":
-      if (id) {
-        app.innerHTML = GrantDetailView();
-        await bindGrantDetail(user, id);
-      } else {
-        app.innerHTML = GrantsListView();
-        await bindGrantsList(user);
-      }
+      if(id){ app.innerHTML = GrantDetailView(); await bindGrantDetail(id); }
+      else { app.innerHTML = GrantsListView(); await bindGrantsList(); }
+      break;
+    case "admin":
+      if(id === "new-grant"){ app.innerHTML = GrantFormView(); bindGrantForm(); }
+      else if(id === "csv"){ app.innerHTML = CsvView(); bindCsv(); }
+      else { location.hash = "#/dashboard"; }
       break;
     default:
       location.hash = "#/dashboard";
   }
 }
 
-window.addEventListener("hashchange", () => navigate(location.hash));
+window.addEventListener("hashchange", ()=>navigate(location.hash));
