75160f375c7b2ec739226e03089c13c1f55fccff
leoavce <hrdeveloper.intern@gmail.com>
2025-10-22 16:36:39 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,34 +1,23 @@
 // js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// TODO: 프로젝트 생성 후 아래 값 교체
+// TODO: 실제 프로젝트 설정으로 교체
 const firebaseConfig = {
   apiKey: "AIzaSyDkHDnimtrct-WAT93jjA0h5QGXISrYElc",
   authDomain: "stock-based-dash.firebaseapp.com",
-  projectId: "stock-based-dash",
-  storageBucket: "stock-based-dash.firebasestorage.app",
-  messagingSenderId: "54158559433",
+  projectId: "stock-based-dash"
   appId: "1:54158559433:web:04ee1be85107804c596518"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+await setPersistence(auth, browserLocalPersistence);
 export const db = getFirestore(app);
 
-// 컬렉션 명 상수
 export const COL = {
-  USERS: "users",
+  EMP: "employees",
   GRANTS: "stock_grants",
-  VESTS: "vesting_schedules",
-  TAX: "tax_events",
-  PRICE: "price_snapshots"
-};
-
-// 역할 클레임 키 (Custom Claims)
-export const ROLES = {
-  EMPLOYEE: "Employee",
-  HR_ADMIN: "HR_Admin",
-  FINANCE: "Finance",
+  VESTS: "vesting_schedules"
 };
