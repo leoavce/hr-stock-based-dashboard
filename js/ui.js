263ae7e127255c17d2276ffefe02313e6f953ab4
leoavce <hrdeveloper.intern@gmail.com>
2025-10-22 15:37:38 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,17 +1,17 @@
 // js/ui.js
-export function toast(msg, ms = 2200) {
+export function toast(msg, ms=2000){
   const el = document.getElementById("toast");
   el.textContent = msg;
   el.classList.remove("hidden");
-  setTimeout(() => el.classList.add("hidden"), ms);
+  setTimeout(()=>el.classList.add("hidden"), ms);
 }
 
-export function spinner(on = true) {
+export function spinner(on=true){
   const app = document.getElementById("app");
-  if (on) {
+  if(on){
     app.innerHTML = `
-      <div class="w-full min-h-[40vh] flex items-center justify-center">
-        <div class="animate-spin rounded-full h-10 w-10 border-4 border-primary border-t-transparent"></div>
-      </div>`;
+      <section class="w-full min-h-[40vh] flex items-center justify-center">
+        <div class="animate-spin rounded-full h-12 w-12 border-4 border-primary border-t-transparent"></div>
+      </section>`;
   }
 }
